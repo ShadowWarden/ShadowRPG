c2f9f71965bc38959dec4e641a079a7b8cf601a9
ShadowWarden <omkar.ramachandran@gmail.com>
2015-12-07 00:55:06 -0700

    v0.1 : Basic Map IO done. Fixing typo in header

--- src/map.h
+++ src/map.h
@@ -8,7 +8,7 @@
 * 
 *
 *  This project is under the GNU General GPL version 3.0 license. For more 
-*  information about the license, refer the LICENCE file in the root directory 
+*  information about the license, refer the LICENSE file in the root directory 
 *  or go online to <http://www.gnu.org/licenses>
 *  
 *  Any redistribution of this code must contain this header in its entirety
