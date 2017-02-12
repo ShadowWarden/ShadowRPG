5df2adbe3f5515fc38e06cd0eecee4712e58d077
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2017-02-12 20:57:37 +0530

    Buggy attempt to keep track of position. Have to fix it and overhaul the load_map_buffer

--- src/main.c
+++ src/main.c
@@ -26,6 +26,7 @@
 #include <stdbool.h>
 
 // TODO create an enum to map numbers with texture names.
+// Some const int can probably be replaced by defines
 
 // Deciding each tile's width and heigh here. It can be changed in one shot in case we 
 // decide to changed it later on
@@ -35,16 +36,21 @@ const int SCREEN_HEIGHT = 480;
 const int WIDTH_BLOCKS = 64;
 const int HEIGHT_BLOCKS = 48;
 const int TEXTURE_COUNT = 128; // right now equal to ASCII chars
-const short int MAP_BUFFER[2000][2000]; // holding a temporary buffer of 2K x 2k map tiles so that file operations are reduced
+const int MAP_BUFFER_HEIGHT = 2000;
+const int MAP_BUFFER_WIDTH = 2000;
+short int MAP_BUFFER[MAP_BUFFER_HEIGHT][MAP_BUFFER_WIDTH]; // holding a temporary buffer of 2K x 2k map tiles so that file operations are reduced
 const char* MAP_DIR = "../resources/maps/"; // concatenate map instead of having to type it everytime
-
+int PLAYER_X = -1; // map isn't loaded yet
+int PLAYER_Y = -1; // map isn't loaded yet
+char CURRENT_MAP[50] = "nomaploadedyet"; // assuming each map name is within 50 characters. Will be used to detect a change in map using the map name
+const char EMPTY_TILE = '0';
 
 // print map only handles printing. not concerned with the map text file. Have to change it
 // get_map_content gets the current position of the player and gets the needed 1000 * 1000 tiles everytime
 // the user approached the end
 void load_all_textures(SDL_Texture *textures_holder[], SDL_Renderer*);
-void get_map_content(char* map_file, int current_x, int current_y);
-void print_map(char*, SDL_Texture *textures_holder[], SDL_Renderer*, SDL_Rect MAP_DISPLAY[HEIGHT_BLOCKS][WIDTH_BLOCKS]);
+void load_map_buffer(char* map_file);
+void print_map(SDL_Texture *textures_holder[], SDL_Renderer*, SDL_Rect MAP_DISPLAY[HEIGHT_BLOCKS][WIDTH_BLOCKS]);
 void get_input(SDL_Window* window);
 
 int main(int argc, char **argv){
@@ -77,7 +83,7 @@ int main(int argc, char **argv){
             
         	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
         	load_all_textures(textures_holder, renderer);
-			print_map("test.map", textures_holder, renderer, MAP_DISPLAY);
+			print_map(textures_holder, renderer, MAP_DISPLAY);
             SDL_UpdateWindowSurface(window);
            
         }
@@ -107,37 +113,7 @@ void load_all_textures(SDL_Texture *textures_holder[], SDL_Renderer* renderer){
 	textures_holder[52] = IMG_LoadTexture(renderer, "../resources/textures/carbon.png");
 }
 
-void print_map(char *map, SDL_Texture *textures_holder[], SDL_Renderer* renderer, SDL_Rect MAP_DISPLAY[HEIGHT_BLOCKS][WIDTH_BLOCKS]){
-
-	// Read the max dimensions from the first two lines and
-	// then allocate a 2-D array and continue from there
-	
-	FILE *fin = fopen("../resources/maps/test.map", "r");
-	int row_count = 0, col_count = 0;
-	short int map_element;
-	
-	fscanf(fin, "%d %d\n", &row_count, &col_count);
-	// Possible improvements:- Use linked lists in case the map
-	// is scarely filled
-	short int map_representation[row_count][col_count];
-	
-	int row = 0, col = 0;
-	while((map_element = fgetc(fin)) != EOF){	
-		// Reading from file and storing it in a data structure
-		if (map_element == 10){
-			while (col < col_count)
-				map_representation[row][col++] = '0';
-			row++; col = 0;
-		}
-		else{
-			map_representation[row][col++] = map_element;
-		}
-	}
-	// filling the last row with zeroes in case it's empty
-	while(col < col_count)
-		map_representation[row][col++] = '0';
-
-	fclose(fin);
+void print_map(SDL_Texture *textures_holder[], SDL_Renderer* renderer, SDL_Rect MAP_DISPLAY[HEIGHT_BLOCKS][WIDTH_BLOCKS]){
 
 	for (int row_iter = 0; row_iter < row_count; row_iter++) {
 		for (int col_iter = 0; col_iter < col_count; col_iter++) {
@@ -148,7 +124,7 @@ void print_map(char *map, SDL_Texture *textures_holder[], SDL_Renderer* renderer
 
 			map_element = map_representation[row_iter][col_iter];
 			switch (map_element) {
-				case '0':
+				case EMPTY_TILE:
 					break;
 
 				default:
@@ -157,19 +133,56 @@ void print_map(char *map, SDL_Texture *textures_holder[], SDL_Renderer* renderer
 			}			
 
 		}
+	}	
+
+}
+
+void load_map_buffer(char* map_file) {
+
+	// Read the max dimensions from the first two lines and
+	// then allocate a 2-D array and continue from there
+	
+	FILE *fin = fopen("../resources/maps/test.map", "r");
+	int row_count = 0, col_count = 0;
+	short int map_element;
+	
+	fscanf(fin, "%d %d\n", &row_count, &col_count);
+	
+	int row = 0, col = 0;
+	while((map_element = fgetc(fin)) != EOF){	
+		// Reading from file and storing it in a data structure
+		if (map_element == 10){
+			while (col < MAP_BUFFER_WIDTH)
+				MAP_BUFFER[row][col++] = EMPTY_TILE;
+			row++; col = 0;
+		}
+		else{
+			MAP_BUFFER[row][col++] = map_element;
+		}
+	}
+	// filling the last row with zeroes in case it doesn't satify the buffer.
+	while(col < MAP_BUFFER_WIDTH)
+		MAP_BUFFER[row][col++] = EMPTY_TILE;
+
+    // Filling in the rest of the buffer with zeroes
+	col = 0;
+    while(row < MAP_BUFFER_HEIGHT){
+		while(col < MAP_BUFFER_WIDTH)
+			MAP_BUFFER[row][col++] = EMPTY_TILE;
+        row++;
 	}
 
-		
+
+	fclose(fin);
 
 	// Test printing the map
 	FILE *fout = fopen("test.txt", "w");
 	fprintf(fout, "%d %d\n", row_count, col_count);
-	for (int i = 0; i < row_count; i++){
-		for (int j = 0; j < col_count; j++)
-			fprintf(fout, "%c", map_representation[i][j]);
+	for (int i = 0; i < MAP_BUFFER_WIDTH; i++){
+		for (int j = 0; j < MAP_BUFFER_WIDTH; j++)
+			fprintf(fout, "%c", MAP_BUFFER[i][j]);
         fprintf(fout,"\n");
 	}
-		
 
 }
 
@@ -192,18 +205,22 @@ void get_input(SDL_Window *window){
 
                     case SDLK_s:
                     case SDLK_DOWN:
+						PLAYER_Y--;
                     break;
 
                     case SDLK_d:
                     case SDLK_RIGHT:
+						PLAYER_X++;
                     break;
 
                     case SDLK_a:
                     case SDLK_LEFT:
+						PLAYER_X--;
                     break;
 
                     case SDLK_w:
                     case SDLK_UP:
+						PLAYER_Y++;
                     break;
 
                     case SDLK_r:
