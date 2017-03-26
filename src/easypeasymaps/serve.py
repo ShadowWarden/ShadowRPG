c23e87b56d0f330afd788d385a37e90daee5582b
Abhirath <abhiabhiabhi.27@gmail.com>
2017-03-26 18:39:24 +0530

    Fix directory bug. Got ascii code working using JSON and server side rendering

--- src/easypeasymaps/serve.py
+++ src/easypeasymaps/serve.py
@@ -1,13 +1,18 @@
 import json
 import os
-
 import flask
+
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def render_index():
-    return flask.render_template('index.html')
+
+    mapping = []
+    with open('tile_ascii_mapping.json') as mapping_file:    
+        mapping = json.load(mapping_file)
+
+    return flask.render_template('index.html', mapping=mapping)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", debug=True, port=8888, threaded=True)
