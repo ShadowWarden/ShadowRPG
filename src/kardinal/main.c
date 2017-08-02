18e6c786ff0773b5bfc00cae43f2b64ace84ab3f
ShadowWarden <omkar.ramachandran@gmail.com>
2017-08-02 07:20:30 -0600

    Compiles but segfautls

--- src/kardinal/main.c
+++ src/kardinal/main.c
@@ -16,6 +16,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "/home/omkar/programs/gdb-7.2/readline/readline.h"
 #include "kardinal.h"
 
 #define MAX_INPUT 256
@@ -25,18 +26,19 @@ State * Titles;
 
 int main(int argc, char ** argv){
 		/* Declarations */
-		char line[MAX_INPUT];
+		char * line = (char *) NULL;
 		State *Player;
 		int PlayerSize = 0;
 		int if_false_flag = 0;
 		int line_number=0;
 		int scope_level = 0;
+		int debug = 1;
 
-		if(argc<3){
-				printf("2 input parameters needed!\n");
-				exit(1);
-		}
-		FILE * state_in = fopen(argv[2],"r"),
+//		if(argc<3){
+//				printf("2 input parameters needed!\n");
+//				exit(1);
+//		}
+		FILE * state_in = fopen(argv[2],"r");
 			 //  * fin = fopen(argv[1],"r");
 			 /*	int debug=0;
 				if(argc>3){
@@ -51,80 +53,87 @@ int main(int argc, char ** argv){
 				}
 			  */
 			 // Initialize variable stack
-			 SymTable * S = (SymTable *) malloc (sizeof(SymTable));
+		SymTable * S = (SymTable *) malloc (sizeof(SymTable));
 
-		build_states_test(state_in, debug);
+//		build_states_test(state_in, debug);
 		//	dump_states_test(debug);
-		//	add_state_to_player(2,&Player,&PlayerSize);
-		//	printf("Debug : %d %s %d\n",Player[0].id,Player[0].name,Player[0].attribute);
-		fclose(state_in);
-		//	while(fgets(line,MAX_INPUT,fin)){
-		//	In->prev = NULL;
-		int i=0;
-		char ch=line[i];
-		line_number++;	
-		/* Pretty sure my beautiful conditional operator is no longer necessary
-		 *  :'-( - OHR
-		 */
-		//		int size_in = (MAX_INPUT>(sizeof(line)))?MAX_INPUT:sizeof(line);
-		while(ch==' ' || ch=='\t'){
-				i++;
-				ch = line[i];
-		}
-		(debug==1) ? printf("Debug : ch = %c\n",ch) : 0;
-		if(ch == '#' || (if_false_flag==1 && ch!='e')){
-				// Comment and not endif!
-				continue;
-		}
-		(debug==1) ? printf("Debug : Command = %s\n",line) : 0;
-		// Build the command stack
-		Input *In = (Input *) malloc (sizeof(Input));
-		In = build(In,line);
-		//		printf("%s : %d : %d : %s\n\n\n",In->name,In->lvl,In->type,In->prev->name);
+//		add_state_to_player(2,&Player,&PlayerSize);
+		printf("Debug : %d %s %d\n",Player[0].id,Player[0].name,Player[0].attribute);
+//		fclose(state_in);
 
-
-		int res = parse(&In,&Player,&PlayerSize,S,line_number,debug);
-
-		if(res == 201){
-				if_false_flag = 1;
-		}else if(res == -202){
-				/* Endif. Get rid of local symbol table */	
-				if(!if_false_flag && scope_level != 0){
-						if(S->level == scope_level){
-								SymTable *tmp = S;
-								S = S->prev;
-								Free_var(*tmp);
-								free(tmp);
-						}
-						scope_level -= 1;
+		//		line = readline(">")
+		do{
+				//	In->prev = NULL;
+	//			if(line){
+	//				free(line);
+	//				line = (char *) NULL;		
+	//			}
+				line = readline(">");
+				int i=0;
+				char ch=line[i];
+				line_number++;	
+				/* Pretty sure my beautiful conditional operator is no longer necessary
+				 *  :'-( - OHR
+				 */
+				//		int size_in = (MAX_INPUT>(sizeof(line)))?MAX_INPUT:sizeof(line);
+				while(ch==' ' || ch=='\t'){
+						i++;
+						ch = line[i];
 				}
-				if_false_flag = 0;
-		}else if(res == 202){
-				/* If statement. Create local symtable */
-				SymTable * tmp = S;
-				scope_level += 1;
-				S = (SymTable *) malloc (sizeof(SymTable));
-				S->level = scope_level;
-				S->prev = tmp;
-		}else if(res!=1 && res){
-				/* There was an error. Exit */
+				(debug==1) ? printf("Debug : ch = %c\n",ch) : 0;
+				if(ch == '#' || (if_false_flag==1 && ch!='e')){
+						// Comment and not endif!
+						continue;
+				}
+				(debug==1) ? printf("Debug : Command = %s\n",line) : 0;
+				// Build the command stack
+				Input *In = (Input *) malloc (sizeof(Input));
+				In = build(In,line);
+				//		printf("%s : %d : %d : %s\n\n\n",In->name,In->lvl,In->type,In->prev->name);
+
+
+				int res = parse(&In,&Player,&PlayerSize,S,line_number,debug);
+
+				if(res == 201){
+						if_false_flag = 1;
+				}else if(res == -202){
+						/* Endif. Get rid of local symbol table */	
+						if(!if_false_flag && scope_level != 0){
+								if(S->level == scope_level){
+										SymTable *tmp = S;
+										S = S->prev;
+										Free_var(*tmp);
+										free(tmp);
+								}
+								scope_level -= 1;
+						}
+						if_false_flag = 0;
+				}else if(res == 202){
+						/* If statement. Create local symtable */
+						SymTable * tmp = S;
+						scope_level += 1;
+						S = (SymTable *) malloc (sizeof(SymTable));
+						S->level = scope_level;
+						S->prev = tmp;
+				}else if(res!=1 && res){
+						/* There was an error. Exit */
+						Free(In);
+						Free_var(*S);
+						free(S);
+						free(Titles);
+						return -1;
+
+				}
+				(debug == 1) ? printf("Debug : Command stack after parse\n") : 0;
+				print(*In,debug);
+				(debug==1) ? printf("\nDebug : Final Result\n") : 0;
+				print_final(*In, debug);
+				//	scanf("%c",&junk);	
+				(debug==1) ? printf("\nDebug : Size of Player state stack : %d\n",PlayerSize) : 0;
+				print_player_state(Player,PlayerSize,debug);
 				Free(In);
-				Free_var(*S);
-				free(S);
-				free(Titles);
-				return -1;
-
-		}
-
-		(debug == 1) ? printf("Debug : Command stack after parse\n") : 0;
-		print(*In,debug);
-		(debug==1) ? printf("\nDebug : Final Result\n") : 0;
-		print_final(*In, debug);
-		//	scanf("%c",&junk);	
-		(debug==1) ? printf("\nDebug : Size of Player state stack : %d\n",PlayerSize) : 0;
-		print_player_state(Player,PlayerSize,debug);
-		Free(In);
-		//	}	
+		}while(line != NULL);
+	
 		//	print_variable_stack(S);
 		(debug==1) ? printf("Debug : Survived print_variable_stack\n") : 0;
 		Free_var(*S);
